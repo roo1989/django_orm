17ddd53c7bbf8b72a052eacda9e93748e128ecd7
Ragnar Olafsson <ragnar.orn@gmail.com>
2026-01-09 15:57:55 +0000

    add some filtering scripts

---
+++ orm_tutorial/scripts/foreign_key_script.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+from django.contrib.auth.models import User
+from django.db import connection
+
+from orm_tutorial.models import Restaurant, Rating
+
+
+def run():
+    restaurant = Restaurant.objects.first()
+    user = User.objects.first()
+
+    print(Rating.objects.all())
+
+    # Filter queryset based off ratings.
+    print(Rating.objects.filter(rating=3))
+
+    # Use double underscore helper to get all ratings greater than or equal 3.
+    print(Rating.objects.filter(rating__gte=3))
+
+    # Same as above, less than or equal.
+    print(Rating.objects.filter(rating__lte=3))
+
+    # Filter by NOT value.
+    print(Rating.objects.exclude(rating__lte=3))
+
+    pprint(connection.queries)
--- orm_tutorial/scripts/orm_script.py
+++ orm_tutorial/scripts/orm_script.py
@@ -15,9 +15,23 @@ def run():
     # restaurant.save()
 
     restaurants = Restaurant.objects.all()
-    print(restaurants)
+    restaurant = Restaurant.objects.first()
+    restaurant_first_index = Restaurant.objects.all()[0] # Will return query LIMIT 1.
 
+    # Create a restaurant using Restaurant.objects.create()
+    # Restaurant.objects.create(
+    #     name='Pizza Shop',
+    #     date_opened=timezone.now(),
+    #     restaurant_type=Restaurant.TypeChoices.ITALIAN,
+    #     latitude=50.2,
+    #     longitude=50.5
+    # )
+
+    # print(restaurants)
+    # print(restaurant)
+    print(Restaurant.objects.count())
     print(connection.queries)
+
 
 
 
