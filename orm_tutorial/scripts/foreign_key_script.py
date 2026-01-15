7ddc7bc0b241e46fddd6386b2684aadd0de0541c
Ragnar Olafsson <ragnar.orn@gmail.com>
2026-01-15 12:03:06 +0000

    stuff

--- orm_tutorial/scripts/foreign_key_script.py
+++ orm_tutorial/scripts/foreign_key_script.py
@@ -7,24 +7,26 @@ from orm_tutorial.models import Restaurant, Rating
 
 
 def run():
-    print(Rating.objects.all())
+    # print(Rating.objects.all())
 
     # Filter queryset based off ratings.
-    print(Rating.objects.filter(rating=3))
+    # print(Rating.objects.filter(rating=3))
 
     # Use double underscore helper to get all ratings greater than or equal 3.
-    print(Rating.objects.filter(rating__gte=3))
+    # print(Rating.objects.filter(rating__gte=3))
 
     # Same as above, less than or equal.
-    print(Rating.objects.filter(rating__lte=3))
+    # print(Rating.objects.filter(rating__lte=3))
 
     # Filter by NOT value.
-    print(Rating.objects.exclude(rating__lte=3))
+    # print(Rating.objects.exclude(rating__lte=3))
 
     # pprint(connection.queries)
 
     # Get the first restaurant
-    restaurant = Restaurant.objects.first()
-    rating= restaurant.ratings.first()
+    restaurant = Restaurant.objects.prefetch_related('ratings').filter(ratings__isnull=True).first()
 
-    print(f"restaurant: {restaurant.name} rating is {rating.rating}")
+    print(f"restaurant: {restaurant.name} rating is {restaurant.ratings}")
+    pprint(connection.queries)
+
+    pprint(Rating.objects.all())
